c3d6415addd7df832b0a4f7c6a3450118311d726
Jess <gomezjessican@gmail.com>
2018-10-04 12:19:42 -0300

    hasta mitad de video 52

---
+++ 02-spa/src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../service/heroes.service';
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+
+})
+export class HeroesComponent implements OnInit {
+
+  constructor(private _heroesService: HeroesService) {
+   }
+
+  ngOnInit() {
+  }
+
+}
