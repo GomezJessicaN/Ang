f0f2ec37c42f781887c8628b45ddd668eedfae1e
Jess <gomezjessican@gmail.com>
2018-10-05 10:59:09 -0300

    rutas, parametros por url, componente del heroe. Hasta vídeo 57

--- 02-spa/src/app/components/heroes/heroes.component.ts
+++ 02-spa/src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HeroesService } from '../../service/heroes.service';
+import { HeroesService, Heroe } from '../../service/heroes.service';
+import {Router} from '@angular/router';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,10 +9,16 @@ import { HeroesService } from '../../service/heroes.service';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor(private _heroesService: HeroesService) {
-   }
-
-  ngOnInit() {
+  heroes: Heroe[] = [];
+  constructor(private _heroesService: HeroesService, private router:Router
+    ) {
   }
 
+  ngOnInit() {
+this.heroes = this._heroesService.getHeroes();
+// console.log(this.heroes);
+  }
+ verHeroe(idx: number){
+this.router.navigate(['/heroe', idx]);
+ }
 }
