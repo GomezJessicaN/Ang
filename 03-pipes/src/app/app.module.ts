26650e0038d81402e692114565cf899e5f7c7833
Jess <gomezjessican@gmail.com>
2018-10-08 14:24:30 -0300

    sección 5 hasta vídeo 80

---
+++ 03-pipes/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { LOCALE_ID, NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+
+registerLocaleData(localeEs);
+
+@NgModule({
+  imports: [ BrowserModule ],
+  declarations: [
+    AppComponent,
+    CapitalizadoPipe
+  ],
+  providers: [{ provide: LOCALE_ID, useValue: 'es' } ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
