38a6466d5c6fa59481c27e0cc177cabe3962951b
Jess <gomezjessican@gmail.com>
2018-10-30 11:19:43 -0300

    hasta ngswitch

--- 06-miscelaneos/src/app/app.module.ts
+++ 06-miscelaneos/src/app/app.module.ts
@@ -5,16 +5,18 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NgStyleComponent } from './components/ng-style/ng-style.component';
-import { CssComponent } from './components/css/css.component';
 import { ClasesComponent } from './components/clases/clases.component';
+import { ResaltadoDirective } from './directives/resaltado.directive';
+import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NgStyleComponent,
-    CssComponent,
     ClasesComponent,
-    ClasesComponent
+    ClasesComponent,
+    ResaltadoDirective,
+    NgSwitchComponent
   ],
   imports: [
     BrowserModule,
