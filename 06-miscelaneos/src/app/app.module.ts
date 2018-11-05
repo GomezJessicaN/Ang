6f88548b323b5c03b04aa3e137972bbef332b595
Jess <gomezjessican@gmail.com>
2018-11-05 10:28:45 -0300

    finalización de sección 8

--- 06-miscelaneos/src/app/app.module.ts
+++ 06-miscelaneos/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { APP_ROUTING } from './app.routes';
-
 import { AppComponent } from './app.component';
 import { NgStyleComponent } from './components/ng-style/ng-style.component';
 import { ClasesComponent } from './components/clases/clases.component';
@@ -16,6 +15,7 @@ import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.compon
 import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
 import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
