b4f850a25337a6563310433b5985909ec242f3a9
Jess <gomezjessican@gmail.com>
2018-11-06 10:38:12 -0300

    comienzo de authapp de nuevo

--- 07-Authapp/src/app/app.module.ts
+++ 07-Authapp/src/app/app.module.ts
@@ -1,30 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component';
-import { PreciosComponent } from './components/precios/precios.component';
-import { ProtegidaComponent } from './components/protegida/protegida.component';
-
-import { APP_ROUTING } from './app.routes';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent,
-    PreciosComponent,
-    ProtegidaComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
-    APP_ROUTING
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
