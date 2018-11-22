62a12ca37b1ea8d62ee9c6166b11928bab341f43
Jess <gomezjessican@gmail.com>
2018-11-22 10:57:54 -0300

    avance hasta vídeo 178

--- 08-formularios/src/app/components/data/data.component.ts
+++ 08-formularios/src/app/components/data/data.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms'
 
 @Component({
   selector: 'app-data',
@@ -10,19 +10,56 @@ export class DataComponent {
 
  forma: FormGroup;
 
+ usuario:any = {
+   nombrecompleto: {
+     nombre:"Marta",
+     apellido:"Del valle"
+   },
+   correo:"Marta.delvalle@gmail.com",
+  // pasatiempos: ["Correr", "Dormir", "Comer"]
+ }
   constructor() {
+ console.log(this.usuario);
 
 this.forma = new FormGroup({
-  'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
-  'apellido': new FormControl('', Validators.required),
-  'correo': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
-});
 
+  'nombrecompleto': new FormGroup({
+    // this.usuario.nombrecompleto.nombre
+    'nombre': new FormControl( '' , [Validators.required, Validators.minLength(3)]),
+    'apellido': new FormControl('' , [Validators.required, this.noHerrera])
+  }),
+  'correo': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]),
+  'pasatiempos':new FormArray([
+    new FormControl('Correr', Validators.required)
+  ])
+});
+ //this.forma.setValue( this.usuario);
   }
+agregarPasatiempo(){
+  (<FormArray>this.forma.controls['pasatiempos']).push(
+    new FormControl('', Validators.required)
+  )
+}
+
+noHerrera(control: FormControl): {[s:string]:boolean}{
+if(control.value === "herrera"){
+  return{
+    noherrera:true
+  }
+}
+return null;
+}
+
 guardarCambios() {
   console.log(this.forma.value);
   console.log(this.forma);
-}
+  this.forma.reset({
+  //  nombrecompleto:{
+  //    nombre:"",
+  //    apellido:""
+  //  },
+  //  correo:""
+  // });
 
 
 }
