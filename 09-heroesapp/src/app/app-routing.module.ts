81a4d1bf4f88d2d5572c4d5213b04881a83e3c52
Jess <gomezjessican@gmail.com>
2018-11-28 10:04:53 -0300

    hasta 187

--- 09-heroesapp/src/app/app-routing.module.ts
+++ 09-heroesapp/src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from './components/heroes/heroe.component';
+import { NgModule } from '@angular/core';
+
 
 
 const app_routes: Routes = [
 { path: 'heroes', component: HeroesComponent },
-{ path: 'heroe/:id', component: HeroeComponent }
+{ path: 'heroe/:id', component: HeroeComponent },
 { path: '**', pathMatch: 'full', redirectTo: 'heroes' }
 
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
+NgModule({
+  imports: [RouterModule.forRoot(app_routes)],
   exports: [RouterModule]
 })
 export  const APP_ROUTING = RouterModule.forRoot(app_routes);
